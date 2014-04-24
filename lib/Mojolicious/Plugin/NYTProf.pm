2c3ff61d36cf15f061e03a4bb2a05c22d5db1bba
Lee Johnson <lee@givengain.ch>
2014-04-24 15:28:41 +0200

    finish up version 0.01
    add dependencies to Makefile.PL and add perldoc. when generating
    profile lists build html rather than using inline templates. put
    profiles in profiles dir to avoid confusion with html profile dirs.
    cleanup files created in testing

--- lib/Mojolicious/Plugin/NYTProf.pm
+++ lib/Mojolicious/Plugin/NYTProf.pm
@@ -11,7 +11,8 @@ Mojolicious::Plugin::NYTProf - Auto handling of Devel::NYTProf in your Mojolicio
 =head1 DESCRIPTION
 
 This plugin enables L<Mojolicious> to automatically generate Devel::NYTProf
-profiles and routes for your app
+profiles and routes for your app, it has been inspired by
+L<Dancer::Plugin::NYTProf>
 
 =head1 SYNOPSIS
 
@@ -19,12 +20,33 @@ profiles and routes for your app
 
   plugin NYTProf => {
     nytprof => {
+      # optional, will default to nytprof
       profiles_dir     => '/some_tmp_dir/',
+      # optional
       nytprofhtml_path => '/path/to/nytprofhtml',
     },
   };
 
   app->start;
+
+Or
+
+  use Mojo::Base 'Mojolicious';
+
+  ...
+
+  sub startup {
+    my $self = shift;
+
+    ...
+
+    my $mojo_config = $self->plugin('Config');
+    # where config contains the necessary keys as show above
+    $self->plugin(NYTProf => $mojo_config);
+  }
+
+Then run your app. Profiles generated can be seen by visting /nytprof and reports
+will be generated on the fly why you click on a specific profile.
 
 =cut
 
@@ -37,8 +59,6 @@ use File::Temp;
 use File::Which;
 use File::Spec::Functions qw/catfile catdir/;
 
-use Data::Dumper;
-
 our $VERSION = '0.01';
 
 =head1 METHODS
@@ -48,14 +68,25 @@ our $VERSION = '0.01';
 Registers the plugin with your app - this will only do something if the nytprof
 key exists in your config hash
 
-  register
-      $self->register($app, \%config);
+  $self->register($app, \%config);
+
+=head1 HOOKS AND Devel::NYTProf
+
+When the nytprof key is missing from your config (or commented out) none of the
+nytprof routes will be loaded and the Devel::NYTProf module will not be imported.
+
+The plugin adds hooks to control the level of profiling, Devel::NYTProf profiling
+is started using a before_routes hook and the stopped with an after_dispatch hook.
+The consequence of this is that you should see profiling only for your routes and
+rendering code and will not see most of the actual Mojolicious framework detail.
 
 =cut
 
 sub register {
   my ($self, $app, $config) = @_;
 
+  # TODO: check mode and do not enable if production (or require a force
+  # config to allow in production)
   if (my $nytprof = $config->{nytprof}) {
 
     my $nytprofhtml_path = $nytprof->{nytprofhtml_path}
@@ -69,9 +100,11 @@ sub register {
     # we need to assign a tmp file and disable profiling from start
     my $prof_dir = $nytprof->{profiles_dir} || 'nytprof';
 
-    if (! -d $prof_dir) {
-      mkdir $prof_dir
-        or die "$prof_dir does not exist and cannot create - $!";
+    foreach my $dir ($prof_dir,catfile($prof_dir,'profiles')) {
+      if (! -d $dir) {
+        mkdir $dir
+          or die "$dir does not exist and cannot create - $!";
+      }
     }
 
     my $tempfh = File::Temp->new(
@@ -79,6 +112,9 @@ sub register {
     );
     my $file      = $tempfh->filename;
     $tempfh       = undef; # let the file get deleted
+
+    # TODO: allow options to be passed for values listed in
+    # https://metacpan.org/pod/Devel::NYTProf#NYTPROF-ENVIRONMENT-VARIABLE
     $ENV{NYTPROF} = "start=no:file=$file";
 
     require Devel::NYTProf;
@@ -94,16 +130,25 @@ sub _add_hooks {
   my $nytprof  = $config->{nytprof};
   my $prof_dir = $nytprof->{profiles_dir} || 'nytprof';
 
+  # add the nytprof/html directory to the static paths
+  # so we can serve these without having to add routes
   push @{$app->static->paths},catfile($prof_dir,'html');
 
-  $app->hook(before_dispatch => sub {
+  # put the actual profile files into a profiles sub directory
+  # to avoid confusion with the *dirs* in nytprof/html
+  my $prof_sub_dir = catfile( $prof_dir,'profiles' );
+
+  # TODO: allow control of hooks with config variables
+  $app->hook(before_routes => sub {
     my $c = shift;
     my $path = $c->req->url->to_string;
     return if $path =~ m{^/nytprof}; # viewing profiles
     $path =~ s!^/!!g;
     $path =~ s!/!-!g;
     my ($sec, $usec) = gettimeofday;
-    DB::enable_profile(catfile($prof_dir,"nytprof_out_${sec}_${usec}_${path}_$$"));
+    DB::enable_profile(
+      catfile($prof_sub_dir,"nytprof_out_${sec}_${usec}_${path}_$$")
+    );
   });
 
   $app->hook(after_dispatch => sub {
@@ -111,50 +156,49 @@ sub _add_hooks {
     DB::finish_profile();
   });
 
-  $app->routes->get('/nytprof/html/:dir'
-    => [dir => qr/nytprof_out_\d+_\d+.*/]
-    => sub { _show_profile(@_,$prof_dir) }
-  );
-
-  $app->routes->get('/nytprof/:file'
+  $app->routes->get('/nytprof/profiles/:file'
     => [file => qr/nytprof_out_\d+_\d+.*/]
     => sub { _generate_profile(@_,$prof_dir,$nytprofhtml_path) }
   );
 
-  $app->routes->get('/nytprof' => sub { _list_profiles(@_,$prof_dir) });
+  $app->routes->get('/nytprof' => sub { _list_profiles(@_,$prof_sub_dir) });
 }
 
 sub _list_profiles {
   my $self = shift;
   my $prof_dir = shift;
 
-  $self->stash(profiles => [_profiles($prof_dir)]);
-  my $ep = <<'EndOfEp';
+  my @profiles = _profiles($prof_dir);
+
+  # could use epl here, but users might be using a different Template engine
+  my $list = @profiles
+    ? '<p>Select a profile run output from the list to view the HTML reports as produced by <tt>Devel::NYTProf</tt>.</p><ul>'
+    : '<p>No profiles found</p>';
+
+  foreach (@profiles) {
+    $list .= qq{
+      <li>
+        <a href="$_->{url}">$_->{label}</a>
+          (PID $_->{pid}, $_->{created}, $_->{duration})
+      </li>
+    };
+  }
+
+  $list .= '</ul>' if $list !~ /No profiles found/;
+
+  my $html = <<"EndOfEp";
 <html>
   <head>
     <title>NYTProf profile run list</title>
   </head>
   <body>
     <h1>Profile run list</h1>
-    % if (@{$profiles}) {
-      <p>Select a profile run output from the list to view the HTML reports as
-  produced by <tt>Devel::NYTProf</tt>.</p>
-      <ul>
-      % for (@{$profiles}) {
-      <li>
-        <a href="<%= $_->{url} %>"><%= $_->{label} %></a>
-          (PID <%= $_->{pid} %>, <%= $_->{created} %>, <%= $_->{duration} %>)
-      </li>
-      % }
-      </ul>
-    % } else {
-      <p>No profiles found</p>
-    %}
+      $list
   </body>
 </html>
 EndOfEp
 
-  $self->render(inline => $ep);
+  $self->render(text => $html);
 }
 
 sub _profiles {
@@ -188,7 +232,7 @@ sub _profiles {
       : '??? seconds - corrupt profile data?';
 
     @{$profile}{qw/file url pid created label/}
-      = ($file,"/nytprof/$file",$pid,scalar localtime($sec),$label);
+      = ($file,"/nytprof/profiles/$file",$pid,scalar localtime($sec),$label);
     push(@profiles,$profile);
   }
 
@@ -201,17 +245,21 @@ sub _generate_profile {
   my $nytprofhtml_path = shift;
 
   my $file    = $self->stash('file');
-  my $profile = catfile($prof_dir, $file);
+  my $profile = catfile($prof_dir,'profiles',$file);
   return $self->render_not_found if !-f $profile;
   
-  foreach my $sub_dir ('html',$file) {
-    $htmldir = catfile($htmldir, $sub_dir);
-
-    if (! -d $htmldir) {
-      mkdir $htmldir
-        or die "$htmldir does not exist and cannot create - $!";
+  foreach my $sub_dir (
+    $htmldir,
+    catfile($htmldir,'html'),
+    catfile($htmldir,'html',$file),
+  ) {
+    if (! -d $sub_dir) {
+      mkdir $sub_dir
+        or die "$sub_dir does not exist and cannot create - $!";
     }
   }
+
+  $htmldir = catfile($htmldir,'html',$file);
 
   if (! -f catfile($htmldir, 'index.html')) {
     system($nytprofhtml_path, "--file=$profile", "--out=$htmldir");
@@ -227,15 +275,7 @@ sub _generate_profile {
     }
   }
 
-  $self->redirect_to("${file}index.html");
-}
-
-sub _show_profile {
-  my $self = shift;
-  my $prof_dir = shift;
-  my $dir = $self->stash('dir');
-
-  $self->redirect_to("${dir}index.html");
+  $self->redirect_to("${file}/index.html");
 }
 
 =head1 AUTHOR
